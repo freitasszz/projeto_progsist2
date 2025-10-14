de90aa4e122e12f56c424d145dbccd5ecd20632d
Gabriel Freitas <gfcmiwa@gmail.com>
2025-10-14 23:02:03 +0000

    inicio projeto

---
+++ proj_estagios/src/main/java/proj_progsist2/proj_estagios/ProjEstagiosApplication.java
@@ -0,0 +1,13 @@
+package proj_progsist2.proj_estagios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjEstagiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjEstagiosApplication.class, args);
+	}
+
+}
---
+++ proj_estagios/src/test/java/proj_progsist2/proj_estagios/ProjEstagiosApplicationTests.java
@@ -0,0 +1,13 @@
+package proj_progsist2.proj_estagios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjEstagiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
